d5534b158caa6f5a81296c79f81ffc6040a3430a
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-11-12 14:46:01 +0000

    improved tests ease

--- Code/src/Tests/Main.java
+++ Code/src/Tests/Main.java
@@ -11,38 +11,12 @@ import GUI.GUI;
 public class Main {
 	public static void main(String[] args) {
 
-		// to see the content of file given in the ^(argument)
-		/*
-		 * GUI window = new GUI("192.168.1.1", 80 , "Code/dl1"); window.open();
-		 */
-		/*
-		 * String workfolder = "./dl1";
-		 * 
-		 * File folder = new File(workfolder);
-		 * 
-		 * File[] files = folder.listFiles(); for (File file : files) { String fileName
-		 * = file.getName(); System.out.println("----------" + fileName + " (size:" +
-		 * file.length() + ")----------");
-		 * 
-		 * // a divisao é dividida em blocos de 10240 bytes (enunciado) e depois o
-		 * ultimo // bloco pelo restante // testar com ficheiros de tamanho diferente
-		 * porque com continhas ja vai lá :] long fileSize = file.length(); int
-		 * blockSize = 10240;
-		 * 
-		 * List<FileBlockRequestMessage> blockList =
-		 * FileBlockRequestMessage.createBlockList(fileName, fileSize, blockSize);
-		 * 
-		 * for (FileBlockRequestMessage block : blockList) { System.out.println("Hash: "
-		 * + block.getHash() + ", Offset: " + block.getOffset() + ", Length: " +
-		 * block.getLength()); } }
-		 */
-
 		// Parse the nodeId from the command-line argument
 		int test = 1;
 		if (test == 1) {
 
 			// Create a Node object with the input nodeId
-			Node no1 = new Node(-1);
+			Node no1 = new Node(1);
 			GUI gui1 = new GUI(no1);
 			gui1.open();
 
@@ -50,13 +24,11 @@ public class Main {
 			GUI gui2 = new GUI(nooo);
 			gui2.open();
 
-		} else {
+		} else if (test == 2) {
 
 			Node no1 = new Node(4);
 			Node no2 = new Node(5);
-			System.out.println("No1 end: " + no1.getEndereco() + " , " + no1.getPort());
-			System.out.println("No2 end: " + no2.getEndereco() + " , " + no2.getPort());
-
+			
 			// Iniciar o servidor do no2 em uma nova thread
 			new Thread(() -> {
 				try {
@@ -76,11 +48,53 @@ public class Main {
 			}
 
 			// Tentar conectar no1 ao no2
+			
 			try {
 				no1.connectToNode("127.0.0.1", 8085);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			
+			
+			try {
+				Thread.sleep(500);
+				no1.sendWordSearchMessageRequest("batata");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+
+		} else if (test == 3) {
+
+			// to see the content of file given in the ^(argument)
+			/*
+			 * GUI window = new GUI("192.168.1.1", 80 , "Code/dl1"); window.open();
+			 */
+			
+			  String workfolder = "./dl1";
+			  
+			  File folder = new File(workfolder);
+			  
+			  File[] files = folder.listFiles(); 
+			  for (File file : files) { 
+				  String fileName = file.getName(); 
+				  System.out.println("----------" + fileName + " (size:" + file.length() + ")----------");
+			  
+			  // a divisao é dividida em blocos de 10240 bytes (enunciado) e depois o ultimo 
+			  // bloco pelo restante 
+			  // testar com ficheiros de tamanho diferente porque com continhas ja vai lá :] 
+			  long fileSize = file.length(); 
+			  int blockSize = 10240;
+			  
+			  List<FileBlockRequestMessage> blockList = FileBlockRequestMessage.createBlockList(fileName, fileSize, blockSize);
+			  
+			  for (FileBlockRequestMessage block : blockList) { 
+				  System.out.println("Hash: " + block.getHash() + ", Offset: " + block.getOffset() + ", Length: " + block.getLength()); 
+				  } 
+			  }
+			 
 
 		}
 

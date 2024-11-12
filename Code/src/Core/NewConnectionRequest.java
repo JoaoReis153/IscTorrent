8a50646879e9a2a761256487f30490717d6493af
Reis <80530563+JoaoReis153@users.noreply.github.com>
2024-11-12 15:20:52 +0000

    Update newConnectionRequest

--- Code/src/Core/NewConnectionRequest.java
+++ Code/src/Core/NewConnectionRequest.java
@@ -1,5 +1,31 @@
 package Core;
 
-public class NewConnectionRequest {
+import java.io.Serializable;
+import java.net.InetAddress;
 
+public class NewConnectionRequest implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private InetAddress clientAddress;
+	private int clientPort;
+	private Connection connection;
+
+	NewConnectionRequest(Connection connection, InetAddress clientAddress, int clientPort) {
+		this.clientAddress = clientAddress;
+		this.clientPort = clientPort;
+		this.connection = connection;
+	}
+	
+	public InetAddress getClientAddress() {
+		return clientAddress;
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	public int getClientPort() {
+		return clientPort;
+	}
+	
+	
 }
